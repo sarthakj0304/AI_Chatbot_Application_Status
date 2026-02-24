8a42a1231bcee4ef80cf43ead3d0797c8847c605
sarthakj0304 <122384746+sarthakj0304@users.noreply.github.com>
2026-02-24 18:19:17 +0530

    frontend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,14 +1,17 @@
 export const sendMessage = async (query) => {
-  const res = await fetch("http://127.0.0.1:8000/chat", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ query }),
-  });
+  const res = await fetch(
+    "https://ai-chatbot-application-status.onrender.com/chat",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query }),
+    },
+  );
   return res.json();
 };
 
 export const sendLead = async (email, role) => {
-  await fetch("http://127.0.0.1:8000/lead", {
+  await fetch("https://ai-chatbot-application-status.onrender.com/lead", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, role }),
@@ -16,11 +19,15 @@ export const sendLead = async (email, role) => {
 };
 
 export const getLogs = async () => {
-  const res = await fetch("http://127.0.0.1:8000/admin/logs");
+  const res = await fetch(
+    "https://ai-chatbot-application-status.onrender.com/admin/logs",
+  );
   return res.json();
 };
 
 export const getStats = async () => {
-  const res = await fetch("http://127.0.0.1:8000/admin/stats");
+  const res = await fetch(
+    "https://ai-chatbot-application-status.onrender.com/admin/stats",
+  );
   return res.json();
 };
